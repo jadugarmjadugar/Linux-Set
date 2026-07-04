802ca7f07b0d2b42ed0227492b4dbadc7861c3c8
Karan Gupta <86535184+jadugarmjadugar@users.noreply.github.com>
2026-07-04 11:22:44 +0530

    Add datatype examples in datatype.py
    Added examples of list, tuple, set, and dictionary usage in Python.

---
+++ datatype.py
@@ -0,0 +1,19 @@
+#list
+abc = ["abc", "def", "xyz", "xyz"]
+#print(len(abc))
+abc[0]=40
+print(abc)
+
+#tuple
+k= ("abc", "def", "xyz", "abc")
+#print(len(def))
+#k[0]= "mno"
+print(k)
+
+#set
+n= {"abc", "def", "xyz", "abc"}
+print(n)
+
+#dictionary
+d = {1: "karan", 2: "abc", 3: "new"}
+print(d[2])
